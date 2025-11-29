d723052a285cc581d32a401c051ecd616e53318f
Edward Källstedt <edward.kallstedt@gmail.com>
2025-11-29 22:54:22 +0100

    set targetsdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -6,16 +8,14 @@ plugins {
 
 android {
     namespace = "dev.minlauncher"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "dev.minlauncher"
         minSdk = 27
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
-
-        resourceConfigurations += listOf("en", "sv")
     }
 
     buildTypes {
@@ -38,13 +38,19 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     buildFeatures {
         viewBinding = true
         buildConfig = true
+    }
+
+    androidResources {
+        localeFilters += listOf("en", "sv")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
 
